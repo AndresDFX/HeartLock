eb264791b8b2a16537c8afb48ac2628e7e9c0cf7
unknown <andres_dfx@hotmail.com>
2020-04-12 19:10:07 -0500

    Fix typo

--- PART3_server.sh
+++
@@ -1,8 +0,0 @@
-#PART3 - Server MPI (SSH For user Vagrant)
-sudo apt-get install -y mpich
-sudo sed -i '/PermitRootLogin.*/c\PermitRootLogin yes' /etc/ssh/sshd_config
-sudo sed -i '/PasswordAuthentication.*/c\PasswordAuthentication yes' /etc/ssh/sshd_config
-sudo service ssh restart
-ssh-keygen -t rsa -q -f "/home/vagrant/.ssh/id_rsa" -N ""
-chown -R vagrant:vagrant /home/vagrant/.ssh
-
